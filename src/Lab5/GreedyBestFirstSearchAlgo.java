206ea817825bce20902c185892573b37c07a3ed1
quoc <quoc@mlnhquxc>
2023-11-06 23:42:42 +0700

    Lab5

---
+++ src/Lab5/GreedyBestFirstSearchAlgo.java
@@ -0,0 +1,46 @@
+package Lab5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class GreedyBestFirstSearchAlgo implements IPuzzleAlgo{
+
+	@Override
+	public Node execute(Puzzle model) {
+		PriorityQueue<Node> frontier = new PriorityQueue<Node>(PuzzleUtils.HeuristicComparatorByH);
+		List<Node> explored = new ArrayList<Node>();
+		
+		Node startNode = new Node(model.getInitialState());
+		startNode.setH(model.computeH2(startNode));
+		frontier.add(startNode);
+		
+		while(!frontier.isEmpty()) {
+			Node currentNode = frontier.poll();
+			explored.add(currentNode);
+			if(currentNode.getH() == 0) {
+				return currentNode;
+			}
+			else {
+				for (Node childNode : model.getSuccessors(currentNode)) {
+					if(!frontier.contains(childNode) && !explored.contains(childNode)){
+						childNode.setG(currentNode.getG()+1);
+						frontier.add(childNode);
+					}
+					else if (frontier.contains(childNode) && !explored.contains(childNode)) {
+						for (Node node : explored) {
+							if(currentNode.equals(node) && currentNode.getH()<node.getH()) {
+								frontier.remove(node);
+								frontier.add(childNode);
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		return null;
+	}
+
+}
